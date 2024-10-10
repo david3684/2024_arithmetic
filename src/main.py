023bee3b83211129f0b92968ca07fc6bac2f488a
david3684 <chan3684@gmail.com>
2024-10-10 13:09:04 +0000

    main formatting done

--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ from src.modeling import ImageEncoder, ImageClassifier
 from src.task_vectors import TaskVector
 from args import parse_arguments
 from src.utils import cosine_lr, LabelSmoothing
+import os
+from src.eval import evaluate, eval_single_dataset
 
 def transform_key(old_key):
     if old_key.startswith('shared.attn.layer'):
         parts = old_key.split('.')
         layer_idx = parts[3]
+        # print(layer_idx)
         sub_key = parts[4]
         
         if sub_key in ['q', 'k', 'v']:
@@ -26,70 +29,95 @@ def main(args):
     
     # Config
     shared_weight = torch.load(args.shared_weight)
-    org_openclip_weight = torch.load(f'checkpoints/{args.model}/zeroshot.pt').state_dict()
-    task1_weight = torch.load('/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/DTD/finetuned.pt').state_dict()
-    task2_weight = torch.load('/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/SUN397/finetuned.pt').state_dict()
+    pretrained_checkpoint = f'checkpoints/{args.model}/zeroshot.pt'
     
-    # shared_weight_state_dict = shared_weight['model']
-    
-    qkv_store = {}
-    new_checkpoint = {}
-    
-    for old_key, value in shared_weight.items():
-        if old_key == 'scale_dict':
-            continue
-        if 'diff' in old_key:
-            continue
-        print(f"transforming {old_key}")
-        new_key = transform_key(old_key)
-        if 'in_proj_weight' in new_key:
-            layer_idx = new_key.split('.')[3]
-            if layer_idx not in qkv_store:
-                qkv_store[layer_idx] = {'q': None, 'k': None, 'v': None}
-            if 'q' in old_key:
-                qkv_store[layer_idx]['q'] = value
-            elif 'k' in old_key:
-                qkv_store[layer_idx]['k'] = value
-            elif 'v' in old_key:
-                qkv_store[layer_idx]['v'] = value
-        else:
-            org_openclip_weight[new_key] = value
-    
-    for layer_idx, qkv in qkv_store.items():
-        if qkv['q'] is not None and qkv['k'] is not None and qkv['v'] is not None:
-            in_proj_weight = torch.cat([qkv['q'], qkv['k'], qkv['v']], dim=0)
-            new_key = f'model.visual.transformer.resblocks.{layer_idx}.attn.in_proj_weight' #concat qkv into 3072*1024 tensor
-            org_openclip_weight[new_key] = in_proj_weight
-
-    for scale_dict_key, value in shared_weight['scale_dict'].items():
-        transformed_scale_dict_key = transform_key(scale_dict_key)
-        print(f'transformed_scale_dict_key: {transformed_scale_dict_key}')
-        if 'clip_vit_1' in scale_dict_key:
-            task1_weight[transformed_scale_dict_key + '.scale'] = value
-        elif 'clip_vit_2' in scale_dict_key:
-            task2_weight[transformed_scale_dict_key + '.scale'] = value
-        elif 'shared' in scale_dict_key:
-            transformed_scale_dict_key = transform_key(scale_dict_key)
-            org_openclip_weight[transformed_scale_dict_key + '.scale'] = value    
+    formatted_shared_weight_path = f"/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/DTD_SUN_shared_weight_openclip.pt"
+    if os.path.exists(formatted_shared_weight_path):
+        shared_weight_formatted = torch.load(formatted_shared_weight_path)
+        shared_state_dict_formatted = shared_weight_formatted.state_dict()
+    else:
+        shared_weight_formatted = torch.load(pretrained_checkpoint)
+        shared_state_dict_formatted = {}
+        finetuned_state_dict = {}
+        for task in args.tasks:
+            finetuned_state_dict[f'{task}'] = torch.load(f'/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/{task}/finetuned.pt').state_dict()
+        qkv_store = {}
         
+        for old_key, value in shared_weight.items():
+            if old_key == 'scale_dict':
+                continue
+            if 'diff' in old_key:
+                continue
+            new_key = transform_key(old_key)
+            if 'in_proj_weight' in new_key:
+                layer_idx = new_key.split('.')[4]
+                if layer_idx not in qkv_store:
+                    qkv_store[layer_idx] = {'q': None, 'k': None, 'v': None}
+                if 'q' in old_key:
+                    qkv_store[layer_idx]['q'] = value
+                elif 'k' in old_key:
+                    qkv_store[layer_idx]['k'] = value
+                elif 'v' in old_key:
+                    qkv_store[layer_idx]['v'] = value
+            else:
+                shared_state_dict_formatted[new_key] = value
+        
+        for layer_idx, qkv in qkv_store.items():
+            print(layer_idx)
+            if qkv['q'] is not None and qkv['k'] is not None and qkv['v'] is not None:
+                in_proj_weight = torch.cat([qkv['q'], qkv['k'], qkv['v']], dim=0)
+                new_key = f'model.visual.transformer.resblocks.{layer_idx}.attn.in_proj_weight' #concat qkv into 3072*1024 tensor
+                print(new_key)
+                shared_state_dict_formatted[new_key] = in_proj_weight
+        for scale_dict_key, value in shared_weight['scale_dict'].items():
+            transformed_scale_dict_key = transform_key(scale_dict_key)
+            if 'clip_vit_1' in scale_dict_key:
+                finetuned_state_dict['DTD'][transformed_scale_dict_key + '.scale'] = value
+            elif 'clip_vit_2' in scale_dict_key:
+                finetuned_state_dict['SUN397'][transformed_scale_dict_key + '.scale'] = value
+            elif 'shared' in scale_dict_key:
+                transformed_scale_dict_key = transform_key(scale_dict_key)
+                shared_state_dict_formatted[transformed_scale_dict_key + '.scale'] = value    
+        
+        temp_state_dict = shared_weight_formatted.state_dict()
+        temp_state_dict.update(shared_state_dict_formatted)
+        shared_weight_formatted.load_state_dict(temp_state_dict)
+        torch.save(shared_weight_formatted, f"/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/DTD_SUN_shared_weight_openclip.pt")
+    
+    # Obtain task vectors from shared weight and finetuned weights
+    # for key in finetuned_state_dict['SUN397']:
+    #     print(key)
+    
+    low_rank_vectors = {}
+    for task in args.tasks:
+        task_vector_path = f"/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/{task}/vector_from_shared.pt"
+        if os.path.exists(task_vector_path):
+            low_rank_vectors[f'{task}'] = torch.load(task_vector_path)
+        else:
+            print(f"Building task vectors for task {task}")
+            finetuned_state_dict[f'{task}'] = torch.load(f'/data2/david3684/2024_arithmetic/checkpoints/ViT-L-14/{task}/finetuned.pt').state_dict()
+            low_rank_vectors[f'{task}'] = TaskVector(args, shared_state_dict_formatted, finetuned_state_dict[f'{task}'], vector=None)
+            torch.save(low_rank_vectors[f'{task}'], task_vector_path)
 
-    for key in task1_weight:
-        print(key)
+    task_vector_sum = sum(low_rank_vectors.values())
 
-    low_rank_vector_task1= TaskVector(args, org_openclip_weight, task1_weight, vector = None)
-    low_rank_vector_task2= TaskVector(args, org_openclip_weight, task1_weight, vector = None)
-
-    # torch.save(org_openclip_weight, f"{args.save}/{args.save_file_name}")
+    # 잘 구했다 치고 evaluate로    
+    multitask_image_encoder = task_vector_sum.apply_to(shared_weight_formatted, scaling_coef=0.8)
+    
+    for dataset in args.tasks:
+        eval_single_dataset(multitask_image_encoder, dataset, args)
+    
     
 if __name__ == "__main__":
     datasets = ['DTD', 'SUN397']
     model = 'ViT-L-14'
     args = parse_arguments()
-    args.data_location = '/path/to/data'
+    args.data_location = '/data2/david3684/data'
     args.model = model
     args.save = f'checkpoints/{model}'
     args.save_file_name = '_'.join(datasets) + '_shared_openclip.pt'
     args.low_rank_mode = 'SoRA'
+    args.tasks = datasets
     args.initial_rank_ratio = 0.5
     pretrained_checkpoint = f'checkpoints/{model}/zeroshot.pt'
     args.shared_weight = '/data2/david3684/2024_arithmetic/shared_weight/20241007_vanilla/rankmin_config_20241009_uni_vanilla_2.bin'
--- src/task_vectors.py
+++ src/task_vectors.py
@@ -3,10 +3,10 @@ import torch
 def make_task_vector_for_weight(args, finetuned_single_weight, pretrained_single_weight):
     """Create a task vector for a single weight tensor."""
     if args.low_rank_mode == 'SoRA':
-        print(finetuned_single_weight, pretrained_single_weight)
+        #print(finetuned_single_weight, pretrained_single_weight)
         diff = finetuned_single_weight - pretrained_single_weight
         
-        print(f"Shape of diff: {diff.shape}")
+        #print(f"Shape of diff: {diff.shape}")
         U, s, V = torch.linalg.svd(diff.to(args.device), full_matrices=False)    
         U, s, V = U.to("cpu"), s.to("cpu"), V.to("cpu")
         dim = s.shape[0]
@@ -21,21 +21,24 @@ def make_task_vector_for_weight(args, finetuned_single_weight, pretrained_single
 def make_task_vector(args, finetuned_state_dict, pretrained_state_dict):
     """Create a task vector from a finetuned and a pretrained tensor."""
     task_vector = {}
+    
     for key in finetuned_state_dict:
-        print(f"Making task vector for {key}")
-        if 'attn' or 'mlp' in key:
+        if 'ln' in key:
+            task_vector[key] = finetuned_state_dict[key] # preserve the layer norm of finetuned weight
+        elif 'bias' in key:
+            task_vector[key] = finetuned_state_dict[key] - pretrained_state_dict[key]  # 이거 pretrained weight 모를 경우에 수정 해줘야 함.
+        elif 'attn' in key or 'mlp' in key:
+            print(f"Making task vector for {key}")
             task_vector[key] = make_task_vector_for_weight(args, finetuned_state_dict[key], pretrained_state_dict[key])
         elif pretrained_state_dict[key].dtype in [torch.int64, torch.uint8]: 
             continue # from original setting
-        elif 'ln' in key:
-            task_vector[key] = finetuned_state_dict[key] # preserve the layer norm of finetuned weight
         else:
             task_vector[key] = finetuned_state_dict[key] - pretrained_state_dict[key]  # 이거 pretrained weight 모를 경우에 수정 해줘야 함.
             
     return task_vector
 
 class TaskVector():
-    def __init__(self, args, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None):
+    def __init__(self, args=None, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None):
         """Initializes the task vector from a pretrained and a finetuned checkpoints.
         
         This can either be done by passing two state dicts (one corresponding to the
@@ -48,6 +51,7 @@ class TaskVector():
             assert pretrained_checkpoint is not None and finetuned_checkpoint is not None
             with torch.no_grad():
                 # pretrained_state_dict = torch.load(pretrained_checkpoint).state_dict()
+                
                 pretrained_state_dict = pretrained_checkpoint
                 finetuned_state_dict = finetuned_checkpoint
                 self.vector = make_task_vector(args, finetuned_state_dict, pretrained_state_dict)
@@ -80,15 +84,22 @@ class TaskVector():
     def apply_to(self, pretrained_checkpoint, scaling_coef=1.0):
         """Apply a task vector to a pretrained model."""
         with torch.no_grad():
-            pretrained_model = torch.load(pretrained_checkpoint)
+            if isinstance(pretrained_checkpoint, str):
+                pretrained_model = torch.load(pretrained_checkpoint)
+                pretrained_state_dict = pretrained_model.state_dict()
+            elif isinstance(pretrained_checkpoint, torch.nn.Module):
+                pretrained_model = pretrained_checkpoint
+                pretrained_state_dict = pretrained_model.state_dict()
+            else:
+                raise ValueError("pretrained_checkpoint must be a file path or a model")
             new_state_dict = {}
-            pretrained_state_dict = pretrained_model.state_dict()
             for key in pretrained_state_dict:
-                if key not in self.vector:
-                    print(f'Warning: key {key} is present in the pretrained state dict but not in the task vector')
-                    continue
-                # add up할때는 bias ln 어떻게 해야 하나...
-                new_state_dict[key] = pretrained_state_dict[key] + scaling_coef * self.vector[key]
-        pretrained_model.load_state_dict(new_state_dict, strict=False)
-        return pretrained_model
+                if key in self.vector:
+                    new_state_dict[key] = pretrained_state_dict[key] + scaling_coef * self.vector[key]
+                else:
+                    new_state_dict[key] = pretrained_state_dict[key]
+
+            # 새로운 state_dict를 모델에 로드
+            pretrained_model.load_state_dict(new_state_dict)
+            return pretrained_model
 
